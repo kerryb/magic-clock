3f02ef3b7d9e9393d68083684513c4e3923312a0
Kerry Buckley <kerryjbuckley@gmail.com>
2012-04-28 17:00:27 +0100

    Don't skip authorisation. Default to mortal peril.

--- app.rb
+++ app.rb
@@ -22,7 +22,6 @@ end
 use Rack::Session::Pool, :expire_after => 86400 # 1 day
 
 before do
-  return
   uri = URI.parse(ENV["MONGOHQ_URL"])
   conn = Mongo::Connection.from_uri(ENV["MONGOHQ_URL"])
   @db = conn.db(uri.path.gsub(/^\//, ""))
@@ -62,15 +61,16 @@ get "/test" do
 end
 
 get "/" do
+  erb :index
+end
+
+get "/angle" do
   result = @client.execute(:api_method => @latitude.current_location.get,
                            :parameters => {"granularity" => "best"})
   response = JSON.parse result.response.body
   lat = response["data"]["latitude"]
   long = response["data"]["longitude"]
-  erb :index
-end
 
-get "/angle" do
   content_type "text/plain"
-  "-60"
+  "60"
 end
