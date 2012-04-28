cbecf92af4d87eea38bcbf37ba2c9c1bbd0112c0
Kerry Buckley <kerryjbuckley@gmail.com>
2012-04-28 19:57:22 +0100

    Remove debug

--- public/js/site.js
+++ public/js/site.js
@@ -1,6 +1,5 @@
 function get_angle() {
   $.get("/angle", function(angle) {
-    console.log("Received angle " + angle);
     $("#hand").css("-webkit-transform", "rotate(" + angle + "deg)").
       css("-moz-transform", "rotate(" + angle + "deg)").
       css("-ms-transform", "rotate(" + angle + "deg)").
