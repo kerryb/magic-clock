8b0ee528f7dec881e278b03a2792752a83a32824
Kerry Buckley <kerryjbuckley@gmail.com>
2012-04-28 19:34:03 +0100

    Add other browser prefixes

--- public/js/site.js
+++ public/js/site.js
@@ -1,7 +1,11 @@
 function get_angle() {
   $.get("/angle", function(angle) {
     console.log("Received angle " + angle);
-    $("#hand").css("-webkit-transform", "rotate(" + angle + "deg)");
+    $("#hand").css("-webkit-transform", "rotate(" + angle + "deg)").
+      css("-moz-transform", "rotate(" + angle + "deg)").
+      css("-ms-transform", "rotate(" + angle + "deg)").
+      css("-o-transform", "rotate(" + angle + "deg)").
+      css("transform", "rotate(" + angle + "deg)");
   });
   setTimeout("get_angle()", 10000);
 }
@@ -9,7 +13,11 @@ function get_angle() {
 $(document).ready(function(){
   $(".location").each(function(){
     var angle = $(this).attr("data-rotation");
-    $(this).css("-webkit-transform", "rotate(" + angle + "deg)");
+    $(this).css("-webkit-transform", "rotate(" + angle + "deg)").
+      css("-moz-transform", "rotate(" + angle + "deg)").
+      css("-ms-transform", "rotate(" + angle + "deg)").
+      css("-o-transform", "rotate(" + angle + "deg)").
+      css("transform", "rotate(" + angle + "deg)");
   });
 
   get_angle();
