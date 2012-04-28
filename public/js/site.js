13b1fd43688c34ec1de3a64f788baa71e41804aa
Kerry Buckley <kerryjbuckley@gmail.com>
2012-04-28 18:10:44 +0100

    Poll for hand angle

--- public/js/site.js
+++ public/js/site.js
@@ -1,10 +1,19 @@
+function get_angle() {
+  $.get("/angle", function(angle) {
+    console.log("Received angle " + angle);
+    $("#hand").css("-webkit-transform", "rotate(" + angle + "deg)");
+  });
+}
+
+function poll() {
+  $.doTimeout(60000, get_angle);
+}
+
 $(document).ready(function(){
   $(".location").each(function(){
     var angle = $(this).attr("data-rotation");
     $(this).css("-webkit-transform", "rotate(" + angle + "deg)");
   });
 
-  $.get("/angle", function(angle) {
-    $("#hand").css("-webkit-transform", "rotate(" + angle + "deg)");
-  });
+  poll();
 });
